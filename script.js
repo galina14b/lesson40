6c0a5d0a251ddd643d40831d69d3c20c5d185310
Halyna <galina14b@gmail.com>
2022-02-07 18:18:12 +0200

    add completed work

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-let card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-    card.classList.toggle('isFlipped');
-});
+let card = document.querySelectorAll('.card');
 
+for (let i = 0; i < card.length; i++) {
+    card[i].addEventListener( 'click', function() {
+        this.classList.toggle('isFlipped');
+    });
+}
+// card.addEventListener( 'click', function() {
+//     card.classList.toggle('isFlipped');
+// });
+//
+//
+//
+//
+// var inputs = document.querySelectorAll("input[type=text]")
+// for (i = 0; i < inputs.length; i++) {
+//     inputs[i].addEventListener('click', function() {
+//         this.style.width = "500px";
+//     });
+// }
