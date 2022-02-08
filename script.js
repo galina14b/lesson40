b1ad2b2f491aeae5949b220750a8ac3096c71a36
Halyna <galina14b@gmail.com>
2022-02-08 14:22:29 +0200

    add changed script

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let card = document.querySelectorAll('.card');
 
 for (let i = 0; i < card.length; i++) {
-    card[i].addEventListener( 'click', function() {
+    card[i].addEventListener( 'mouseenter', function() {
+        this.classList.toggle('isFlipped');
+    });
+    card[i].addEventListener( 'mouseleave', function() {
         this.classList.toggle('isFlipped');
     });
 }
